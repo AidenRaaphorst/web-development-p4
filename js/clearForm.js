ab74614a0f41a9e79b3070efe37a0067fcf72ab1
AidenRaaphorst <1197043@student.roc-nijmegen.nl>
2022-06-13 12:14:04 +0200

    Clear knop geeft geen error meer

--- js/clearForm.js
+++ js/clearForm.js
@@ -2,14 +2,16 @@ const clearButton = document.querySelector(".clear-form");
 
 clearButton.addEventListener("click", (e) => {
 	const textInputs = document.querySelectorAll(".create-update input[type=text]");
-	const textAreas = document.querySelector(".create-update textarea");
+	const textAreas = document.querySelectorAll(".create-update textarea");
 	const submitButton = document.querySelector(".create-update input[type=submit]");
 
 	textInputs.forEach((input) => {
 		input.value = "";
 	});
 
-	textAreas.value = "";
+	textAreas.forEach((textArea) => {
+		textArea.value = "";
+	});
 
 	submitButton.name = "create";
 	submitButton.value = "Create";
